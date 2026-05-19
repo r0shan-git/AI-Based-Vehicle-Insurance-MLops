556dcf2d1101398522705b8e55609c258e443393
r0shan-git <roshan10072004@gmail.com>
2026-05-19 17:24:41 +0530

    project structure ready ,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Roshan Gupta",
+    author_email="roshangupta10072004@gmail.com",
+    packages=find_packages()
+)
