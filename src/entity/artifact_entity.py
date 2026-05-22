48c25ae14a48136cabd69eb006b01d15b60c69d8
r0shan-git <roshan10072004@gmail.com>
2026-05-22 14:03:41 +0530

    my model deployment

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -6,20 +6,17 @@ class DataIngestionArtifact:
     trained_file_path:str 
     test_file_path:str
 
-
 @dataclass
 class DataValidationArtifact:
     validation_status:bool
     message: str
     validation_report_file_path: str
 
-
 @dataclass
 class DataTransformationArtifact:
     transformed_object_file_path:str 
     transformed_train_file_path:str
     transformed_test_file_path:str
-
 
 @dataclass
 class ClassificationMetricArtifact:
@@ -31,3 +28,15 @@ class ClassificationMetricArtifact:
 class ModelTrainerArtifact:
     trained_model_file_path:str 
     metric_artifact:ClassificationMetricArtifact
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
+
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str
